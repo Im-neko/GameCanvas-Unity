f905d5ebe7126f6e22f54a764a5594345d315ba7
im-neko <lukepuyuki0430@gmail.com>
2018-10-20 23:20:05 +0900

    kadai

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -9,6 +9,22 @@ public sealed class Game : GameBase
 {
     // 変数の宣言
     int sec = 0;
+    int ball_x;
+    int ball_y;
+    int ball_speed_x;
+    int ball_speed_y;
+    int player_x;
+    int player_y;
+    int player_w;
+    int player_h;
+    const int BLOCK_NUM = 50;
+    int blocks = 50;
+    int[] block_x = new int [BLOCK_NUM];
+    int[] block_y = new int [BLOCK_NUM];
+    bool[] block_alive_flag = new bool [BLOCK_NUM];
+    int block_w = 64;
+    int block_h = 20;
+    int time ;
 
     /// <summary>
     /// 初期化処理
@@ -17,6 +33,22 @@ public sealed class Game : GameBase
     {
         // キャンバスの大きさを設定します
         gc.SetResolution(720, 1280);
+        gc.SetResolution(640, 480);
+        ball_x = 0;
+        ball_y = 0;
+        ball_speed_x = 3;
+        ball_speed_y = 3;
+        player_x = 270;
+        player_y = 460;
+        player_w = 100;
+        player_h = 20;
+        for(int i =0 ; i < BLOCK_NUM ; i ++ )
+        {
+            block_x[i] = (i % 10 ) * block_w;
+            block_y[i] = (i / 10 ) * block_h;
+            block_alive_flag[i] = true;
+        }
+        time = 0;
     }
 
     /// <summary>
@@ -26,6 +58,53 @@ public sealed class Game : GameBase
     {
         // 起動からの経過時間を取得します
         sec = (int)gc.TimeSinceStartup;
+        ball_x = ball_x + ball_speed_x;
+        ball_y = ball_y + ball_speed_y;
+
+        if (gc.GetPointerFrameCount(0) > 0 ){
+            player_x = gc.GetPointerX(0) - player_w/2;
+            player_y = gc.GetPointerY(0) - player_h/2;
+        }
+        
+        if (gc.CheckHitRect(ball_x, ball_y, 24, 24, player_x, player_y, player_w, player_h)) 
+        {
+            if (ball_speed_y > 0) {
+                ball_speed_y = -ball_speed_y;
+            } 
+        }
+
+        if( ball_x < 0 ) {
+          ball_x = 0;
+          ball_speed_x = -ball_speed_x;
+        }
+
+        if( ball_y < 0 ) {
+          ball_y = 0;
+          ball_speed_y = -ball_speed_y;
+        }
+
+        if( ball_x > 616 ) {
+          ball_x = 616;
+          ball_speed_x = -ball_speed_x;
+        }
+
+        if( ball_y > 456 ) {
+          ball_y = 480;
+          ball_speed_y = 0;
+          ball_speed_x = 0;
+        }
+        
+        for (int i=0; i < BLOCK_NUM; i ++ )
+        {
+            if (gc.CheckHitRect(block_x[i], block_y[i], block_w, block_h, ball_x, ball_y, 24, 24) && block_alive_flag[i])
+            {
+                block_alive_flag[i] = false;
+                blocks--;
+            }
+        }
+        if ( blocks > 0) {
+            time++;
+        }
     }
 
     /// <summary>
@@ -39,11 +118,31 @@ public sealed class Game : GameBase
         // 0番の画像を描画します
         gc.DrawImage(0, 0, 0);
 
-        // 黒の文字を描画します
-        gc.SetColor(0, 0, 0);
-        gc.SetFontSize(48);
-        gc.DrawString("この文字と青空の画像が", 40, 160);
-        gc.DrawString("見えていれば成功です", 40, 270);
-        gc.DrawRightString($"{sec}s", 630, 10);
+        gc.DrawImage(1,ball_x,ball_y);
+
+        gc.SetColor(0, 0, 255);
+        gc.DrawString(time.ToString(), 1,1);
+        gc.FillRect(player_x,player_y,player_w,player_h);
+        for (int i=0; i < BLOCK_NUM; i ++ )
+        {
+            if( block_alive_flag[i] )
+            {
+                gc.FillRect(block_x[i], block_y[i], block_w, block_h);
+            }
+        }
+        if (blocks < 1) 
+        {
+            gc.DrawCenterString("Clear!", 100, 230);
+        }
+    }
+
+    int countBlock(){
+        int num = 0;
+        for(int i =0 ; i < BLOCK_NUM ; i ++ ){
+            if(block_alive_flag[i]){
+            num++;
+            }
+        }
+        return num;
     }
 }
