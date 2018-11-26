062812eb39320376e1f924e3f01ad212acf3c3a0
im-neko <lukepuyuki0430@gmail.com>
2018-11-26 22:33:42 +0900

    submit

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -1,4 +1,4 @@
-﻿
+﻿using UnityEngine;
 using Sequence = System.Collections.IEnumerator;
 
 /// <summary>
@@ -8,15 +8,35 @@ using Sequence = System.Collections.IEnumerator;
 public sealed class Game : GameBase
 {
     // 変数の宣言
-    int sec = 0;
+    string pname = "t16076yi";
+    string url = "";
+
+    int gameState = 0;
+
+    const int BOX_NUM = 10;
+    int[] box_x = new int [BOX_NUM];
+    int[] box_y = new int [BOX_NUM];
+    int[] box_speed = new int [BOX_NUM];
+    int box_w = 24;
+    int box_h = 24;
+
+    int player_x = 304;
+    int player_y = 400;
+    int player_dir = 1;
+    int player_speed = 5;
+
+    int score = 0;
+    int count = 0;
+    string str = "";
+
+
 
     /// <summary>
     /// 初期化処理
     /// </summary>
     public override void InitGame()
     {
-        // キャンバスの大きさを設定します
-        gc.SetResolution(720, 1280);
+        resetValue();
     }
 
     /// <summary>
@@ -24,8 +44,49 @@ public sealed class Game : GameBase
     /// </summary>
     public override void UpdateGame()
     {
-        // 起動からの経過時間を取得します
-        sec = (int)gc.TimeSinceStartup;
+        if (gameState == 0) {
+            if(gc.GetPointerFrameCount(0) == 1 ){
+                resetValue();
+                url = "http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/score.cgi?score=-1&name=" + pname;
+                gc.GetOnlineTextAsync(url,out str);
+                gameState = 1;
+            }
+        } else if (gameState == 1) {
+            count++;
+            score = count / 60;
+            box_w = 24 + count / 300;
+            box_h = 24 + count / 300; 
+            if(gc.GetPointerFrameCount(0) == 1 ){
+                player_dir = -player_dir;
+            }
+            player_x += player_dir * player_speed;
+            for(int i =0 ; i < BOX_NUM ; i ++ )
+            {
+                box_y[i] = box_y[i] + box_speed[i];
+                if(box_y[i]> 480){
+                    box_x[i] = gc.Random(0,616);
+                    box_y[i] = -gc.Random(100,480);
+                    box_speed[i] = gc.Random(3,6);
+                }
+                if (gc.CheckHitRect (
+                    player_x,player_y,32,32,
+                    box_x[i],box_y[i],box_w,box_h)) {
+                        gameState = 2;
+                }
+            }
+            if(player_x < 0 || player_x > 608){
+                gameState = 2;
+            } 
+        } else if (gameState == 2) {
+            url = "http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/score.cgi?score="
+                    + score + "&name=" + pname;
+            gc.GetOnlineTextAsync(url,out str);
+            if(gc.GetPointerFrameCount(0) > 120 ){
+                url = "http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/score.cgi?score=-1&name=" + pname;
+                gc.GetOnlineTextAsync(url,out str);
+                gameState = 0;
+            }
+        }
     }
 
     /// <summary>
@@ -33,17 +94,36 @@ public sealed class Game : GameBase
     /// </summary>
     public override void DrawGame()
     {
-        // 画面を白で塗りつぶします
         gc.ClearScreen();
 
-        // 0番の画像を描画します
-        gc.DrawImage(0, 0, 0);
-
-        // 黒の文字を描画します
         gc.SetColor(0, 0, 0);
-        gc.SetFontSize(48);
-        gc.DrawString("この文字と青空の画像が", 40, 160);
-        gc.DrawString("見えていれば成功です", 40, 270);
-        gc.DrawRightString($"{sec}s", 630, 10);
+        if (gameState == 0) {
+            gc.DrawString("Title", 0, 300);
+        } else if (gameState == 1) {
+            gc.DrawString("score" + score, 0, 10);
+            gc.DrawOnlineImage("http://web.sfc.keio.ac.jp/~wadari/sdp/k07_web/Player.png",player_x,player_y);
+            for(int i =0 ; i < BOX_NUM ; i ++ ){
+                gc.FillRect(box_x[i],box_y[i],box_w,box_h);  
+            }
+        } else if (gameState == 2) {
+            gc.DrawString("Game Over", 0, 200);
+            gc.DrawString("score" + score, 0, 300);
+            gc.DrawString(""+str, 0, 400);
+        }
+    }
+
+    public void resetValue() {
+        score = 0;
+        count = 0;
+        player_x = 304;
+        player_y = 400;
+        player_dir = 1;
+        player_speed = 5;
+        gc.SetResolution(639, 480);
+        for(int i =0 ; i < BOX_NUM ; i ++ ) {
+            box_x[i] = gc.Random(0,616);
+            box_y[i] = -gc.Random(100,480);
+            box_speed[i] = gc.Random(3,6);
+        }
     }
 }
